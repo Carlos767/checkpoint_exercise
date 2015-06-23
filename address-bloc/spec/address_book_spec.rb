5a6e6e2f4504d894e0c032da0e0c45cddb35ddf4
Carlos767 <cjparrillasf@gmail.com>
2015-06-23 15:07:59 -0700

    Recovering Checkpoint 21

--- address-bloc/spec/address_book_spec.rb
+++ address-bloc/spec/address_book_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../models/address_book.rb'
 
 RSpec.describe AddressBook do
-  contect "attributes" do
+  context "attributes" do
     it "should respond to entries" do
       book = AdressBook.new
       expect(book).to respond_to(:entries)
@@ -21,14 +21,14 @@ RSpec.describe AddressBook do
   context "add_entry" do
     it "adds only one entry to the address book" do
     book = AddressBook.new
-    book.add_entry('Asa Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
+    book.add_entry('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
     
     expect(book.entries.size).to eq 1
   end
   
   it "adds the correcr information to entries" do
     book = AddressBook.new
-    book.add_entry('Asa Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
+    book.add_entry('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
     new_entry = book.entries[0]
     
     expect(new_entry.name).to eq 'Ada Lovelace'
--- address-bloc/spec/entry_spec.rb
+++ address-bloc/spec/entry_spec.rb
@@ -5,20 +5,25 @@ RSpec.descrbe Entry do
     it "should respond to name" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
       expect(entry).to respond_to(:name)
-      end
+    end
       
-      it "should respond to phone number" do
-        entry = Entry.new('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
-        expect(entry).to respond_to(:phone_number)
-        end
+    it "should respond to phone number" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
+      expect(entry).to respond_to(:phone_number)
+    end
         
-        it "should respond to email" do
-          entry = Entry.new('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
-          expect(entry).to respond_to(:email)
-        end
-      end
+    it "should respond to email" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'agusta.king@lovelace.com')
+      expect(entry).to respond_to(:email)
+    end
+  end
       
-      context ".to_s" do
-        it "prints an entry as a string" do
-          entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-          e
+  context ".to_s" do
+    it "prints an entry as a string" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expected_string = "Name: Ada Lovelace\nPhone number: 010.012.1815\nEmail: agusta.king@lovelace.com"
+
+      expect(entry.to_s).to eq(expected_string)
+    end
+  end
+end
