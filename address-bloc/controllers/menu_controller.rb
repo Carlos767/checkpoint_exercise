9ef5a76b0e6e02ccda2c687500ccdb785ab99bde
Carlos767 <cjparrillasf@gmail.com>
2015-06-23 16:44:31 -0700

    Assignment 22- view entry number n

--- address-bloc/controllers/menu_controller.rb
+++ address-bloc/controllers/menu_controller.rb
@@ -18,6 +18,104 @@ class MenuController
 		print "Enter your selection: "
 
 		selection = gets.to_i
-		puts "You picked #{selection}"
+		case selection
+		when 1
+        	system "clear"
+        	view_all_entries
+        	main_menu
+   		when 2
+        	system "clear"
+        	create_entry
+        	main_menu
+    	when 3
+        	system "clear"
+        	search_entries
+        	main_menu
+    	when 4
+        	system "clear"
+        	read_csv
+        	main_menu
+    	when 5
+        	puts "Good-bye!"
+    	when 6
+    		system "clear"
+    		view_entry_number
+    		main_menu
+
+        	exit(0)
+    	else
+        	system "clear"
+        	puts "Sorry, that is not a valid input"
+        	main_menu
+        end
+    end
+
+	def view_all_entries
+   		@address_book.entries.each do |entry|
+        	sytem "clear"
+        	puts entry.to_s
+
+        	entry_submenu(entry)
+    	end
+
+    	system "clear"
+    	puts "End of entries"
 	end
+
+	def entry_submenu(entry)
+	   puts "n - next entry"
+	   puts "d - delete entry"
+	   puts "e - edit this entry"
+	   puts "m - return to main menu"
+
+	   selection = gets.chomp
+
+	   case selection
+
+	   when "n"
+	   when "d"
+	   when "e"
+	   when "m"
+
+	   system "clear"
+       main_menu
+   else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       entries_submenu(entry)
+    end
+
+    def create_entry
+    	system "clear"
+        puts "new AdderessBloc Entry"
+
+        print "Name: "
+        name = gets.chomp
+        print "Phone number: "
+        phone = gets.chomp
+        print "Email: "
+        email = gets.chomp
+
+        @address_book.add_entry(name, phone, email)
+
+        system "clear"
+        puts "New entry created"
+    end
+
+    def search_entries
+    end
+
+    def read_csv
+    end
+
+    def view_entry_number(n)
+
+    	puts "Select your entry"
+   		print " Your entry is: "
+   		n = gets.chomp
+   	else
+    	system "clear"
+    	puts "#{n} is not a valid input"
+    	view_entry_number(n)
+    end
 end
